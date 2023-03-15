ffad0b82f3fd7d169fa88e13556e6f836efed9ca
danieljuma5 <jumadaniel275@gmail.com>
2023-03-15 14:01:13 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+//divide function expression
+const divide = function (a = 2000,b = 100) {
+  return a / b;
+}
+divide();
+
+//Square function
+
+const square = exp => exp * exp
+
+square(2);
+
+//Arrow  function add
+const add = (a,b) => a + b;
